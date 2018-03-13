2d6e23f56d7173ec9308aef552873947c6a09545
Alexander Urizar <urizaraj@gmail.com>
2018-03-13 15:11:20 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w[Fiction Non-Fiction]}
+  validates_each :title do |record, attr, value|
+    valid = [/Won't Believe/, /Secret/, /Top \d+/, /Guess/].any? do |regex|
+      regex.match(value)
+    end
+  end
 end
