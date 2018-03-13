880b5f839b00cd0f0e6fcf98d486d7c1ff226254
Alexander Urizar <urizaraj@gmail.com>
2018-03-13 15:12:12 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w[Fiction Non-Fiction]}
+  validates_each :title do |record, attr, value|
+    valid = [/Won't Believe/, /Secret/, /Top \d+/, /Guess/].any? do |regex|
+      regex.match(value)
+    end
+    record.errors.add(attr, 'Must be a clickbait title') unless valid
+  end
 end
